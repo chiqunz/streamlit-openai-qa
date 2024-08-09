1598fb042b5414c8e6b3d4d5d4a271920a74552f
Chiqun Zhang <31827519+chiqunz@users.noreply.github.com>
2024-08-09 19:55:07 +0000

    swith to prod secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def generate_messages(task, query, uploaded_file):
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = "" #st.secrets["api"]['open_ai']
+openai_api_key = st.secrets["api"]['open_ai']
 if not openai_api_key:
     st.info("Please add your OpenAI API key in the env.", icon="🗝️")
 
