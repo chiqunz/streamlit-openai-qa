743ce540985098024b12be6447a545fa62880e61
Chiqun Zhang <31827519+chiqunz@users.noreply.github.com>
2024-08-09 06:20:07 +0000

    fix prompt dict bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,40 @@ import streamlit as st
 from openai import OpenAI
 
 predefined_prompts = {
-    "uploaded_file": "Here's a document: {0}",
+    "document_qa": "Here's a document: {0}",
     "polish_email": "Please help polish and rephrase the following email",
     "polish_paragraph": "Please help polish, rephrase, correct grammas for the following paragraphs"
 }
+
+my_tasks = ["polish_email", "polish_paragraph", "document_qa"]
+uploaded_file = None
+
+def generate_messages(task, question, uploaded_file):
+    match task:
+        case 'document_qa':
+            document = uploaded_file.read().decode()
+            messages = [
+                {
+                    "role": "user",
+                    "content": predefined_prompts[task].format(document) + f"\n\n---\n\n {question}",
+                }
+            ]
+        case None:
+            messages = [
+                {
+                    "role": "user",
+                    "content": question,
+                }
+            ]
+        case _:
+            messages = [
+                {
+                    "role": "user",
+                    "content": predefined_prompts[task] + f"\n\n---\n\n {question}",
+                }
+            ]
+    return messages
+
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
@@ -15,7 +45,6 @@ if not openai_api_key:
     st.info("Please add your OpenAI API key in the env.", icon="🗝️")
 
 st.title("📝Tasks")
-my_tasks = ["polish_email", "polish_paragraph", "document_qa"]
 task = st.selectbox("Select a predefined task:", my_tasks, index=None)
 
 if task == "document_qa":
@@ -40,31 +69,7 @@ question = st.text_area(
 )
 
 if question:
-    messages = [
-        {
-            "role": "user",
-            "content": f"{question}",
-        }
-    ]
-
-    if task == "document_qa" and uploaded_file:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": predefined_prompts["uploaded_file"].format(document) + f"\n\n---\n\n {question}",
-            }
-        ]
-    
-    if task:
-        messages = [
-            {
-                "role": "user",
-                "content": predefined_prompts[task] + f"\n\n---\n\n {question}",
-            }
-        ]
+    messages = generate_messages(task, question, uploaded_file)
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
