be314c743ae9b5ff8d9791335741903481921bd1
Chiqun Zhang <31827519+chiqunz@users.noreply.github.com>
2024-08-09 05:43:59 +0000

    use st secret for keys

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,14 @@ from openai import OpenAI
 st.title("📄 Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.secrets["api"]['open_ai']
 if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+    st.info("Please add your OpenAI API key in the env.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
